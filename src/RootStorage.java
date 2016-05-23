cc63504569e9cb4d8f68357128819ba39679d56c
Esteban Garro <egarro@transcriptics.com>
2016-05-22 21:41:28 -0400

    Removes RootStorage unneccessary size property

--- src/RootStorage.java
+++ src/RootStorage.java
@@ -1,30 +1,19 @@
-public class QuadraticRoots {
- private double firstRoot;
- private double secondRoot;
- private boolean isImaginary;
- private boolean isQuadratic;
-public boolean isQuadratic() {
-	return isQuadratic;
-}
-public void setQuadratic(boolean isQuadratic) {
-	this.isQuadratic = isQuadratic;
-}
-public boolean isImaginary() {
-	return isImaginary;
-}
-public void setImaginary(boolean isImaginary) {
-	this.isImaginary = isImaginary;
-}
-public double getFirstRoot() {
-	return firstRoot;
-}
-public void setFirstRoot(double firstRoot) {
-	this.firstRoot = firstRoot;
-}
-public double getSecondRoot() {
-	return secondRoot;
-}
-public void setSecondRoot(double secondRoot) {
-	this.secondRoot = secondRoot;
-}
+public class RootStorage {
+ double[] roots;
+
+ public void setSize(int size) {
+	roots = new double[size+1];
+ }
+ 
+ public void setRoot(int idx, double root) {
+	 this.roots[idx] = root;	 
+ }
+ 
+ public double getRoot(int idx) {
+		return this.roots[idx];
+ }
+ 
+ public double[] getRoots() {
+	 return this.roots;
+ }
 }
