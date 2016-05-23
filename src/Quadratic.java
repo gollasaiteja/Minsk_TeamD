8e13d4b9e5162d455110b4bc9f60369c74b1452f
Esteban Garro <egarro@transcriptics.com>
2016-05-22 21:42:34 -0400

    Refactors Quadratic class to throw custom exception

--- src/Quadratic.java
+++ src/Quadratic.java
@@ -1,42 +1,30 @@
-import java.util.Scanner;
 public class Quadratic {
-		public QuadraticRoots findQuadraticRoots(int a, int b, int c) {
-			QuadraticRoots quadraticRoots = null;
-		 	double firstRoot,secondRoot;
-		 	int discr,total,abs_a,abs_b,abs_c;
-		 	abs_a=(a<0)?-a:a;
-		 	abs_b=(b<0)?-b:b;
-		 	abs_c=(c<0)?-c:c;
-	    	quadraticRoots = new QuadraticRoots();
-	    	if (a==0){
-	    		quadraticRoots.setQuadratic(false);
-	    	}
-	    	else
-	    	{
-	    		discr = (b*b)-(4*a*c);
-	    		if (discr>0){
-	    			Double sqr=findSquareRoot.SquareRoot(discr);
-	    			firstRoot=(-b +sqr)/(2*a);
-	    			secondRoot=(-b - sqr)/(2*a);
-	    			quadraticRoots.setFirstRoot(firstRoot);
-	    			quadraticRoots.setSecondRoot(secondRoot);
-	    		}
-	    		else
-	    		{
-	    			if (discr==0)
-	    			{
-	    				firstRoot = secondRoot = -b/(2*a);
-		    			quadraticRoots.setFirstRoot(firstRoot);
-		    			quadraticRoots.setSecondRoot(secondRoot);
-	    			}
-	    			else
-	    			{
-	    				quadraticRoots.setImaginary(true);
-	    			}
-	    		}
-	    	}
-			return quadraticRoots;
-		}
-	   }
+	public RootStorage findQuadraticRoots(int a, int b, int c) throws WrongParameterException {
+		RootStorage quadraticRoots = new RootStorage();
+    	quadraticRoots.setSize(2);
+    	
+		double firstRoot,secondRoot;
+	 	int discr;
+
+	 	discr = (b*b)-(4*a*c);
+    		if (discr>0) {
+    			Double sqr= SquareRoot.find(discr);
+    			firstRoot=(-b +sqr)/(2*a);
+    			secondRoot=(-b - sqr)/(2*a);
+    			quadraticRoots.setRoot(1,firstRoot);
+    			quadraticRoots.setRoot(2,secondRoot);
+    		}
+    		else {
+    			if (discr==0) {
+    				firstRoot = secondRoot = -b/(2*a);
+	    			quadraticRoots.setRoot(1,firstRoot);
+	    			quadraticRoots.setRoot(2,secondRoot);
+    			} else {
+    				throw new WrongParameterException("The roots of this equation are imaginary!");
+    			}
+    		}
+		return quadraticRoots;
+	}
+}
 
 
