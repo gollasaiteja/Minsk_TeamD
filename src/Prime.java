e384e8826acbe5225ddaef09386ff39e96d8d8be
Esteban Garro <egarro@transcriptics.com>
2016-05-22 21:36:41 -0400

    Refactors the Prime class for faster performance (checks only odd numbers)

--- src/Prime.java
+++ src/Prime.java
@@ -1,21 +1,25 @@
+//This class handles all calculations regarding Primality.
 public class Prime {
-	
-
-	
-	
-	public boolean findPrime(int x)
-	{
-		if(x<0)
-			x=-x;
-	 
-		for( int i=2; i<= x/2; i++ )
-		{
-			if(x % i == 0)
-			{
-				return false;
-			}
-		}
+	//Determines if a given positive number is prime or not:
+	public boolean isPrime(int x) throws WrongParameterException {
+		if(x < 0) {
+			throw new WrongParameterException("Attempting to determine primality of a negative number...");
+		} else if ( x == 1 ) {
+			return false;
+		} else if ( x == 2 ) {
 			return true;
+		} else if ( x%2 == 0 ) {
+			return false;
+		} else {
+			//Main algorithm. Check if a given odd numbers is prime by checking the residue when dividing
+			//by all possible odd numbers:
+			for( int i=3; i <= x/2; ) {
+				if(x % i == 0) {
+					return false;
+				}
+				i += 2;
+			}
+		}		
+		return true;
 	}
-
 }
